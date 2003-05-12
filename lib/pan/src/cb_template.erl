f3ab02e540ee4429023f2c931c7916acc80bbd79
masse <masse>
2003-05-12 07:55:23 +0000

    cosmetic

--- lib/pan/src/cb_template.erl
+++ lib/pan/src/cb_template.erl
@@ -29,19 +29,16 @@
 
 -export([go/1, requires/0, doc/0]).
 
--record(state, {}).
+-record(st, {}).
 
 requires() -> [{flags, []}, {tps, []}].
 doc() -> "prints everything".
 
 go([Message, Line, Out, initial|_]) ->
     io:fwrite(Out, "~p: initializing~n", [?MODULE]),
-    go([Message, Line, Out, initial_state()]);
-go([end_of_trace, Line, Out, State|_]) ->
+    go([Message, Line, Out, #st{}]);
+go([end_of_trace, Line, Out, St|_]) ->
     io:fwrite(Out, "~p: exiting - msgs: ~w~n", [?MODULE, Line-1]);
-go([Message, Line, Out, State|_]) ->
+go([Message, Line, Out, St|_]) ->
     io:fwrite(Out, "~p: ~-5w - ~w~n", [?MODULE, Line, Message]),
-    State.
-
-initial_state() ->
-    #state{}.
+    St.

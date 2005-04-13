36dc1bf005c88b64e564f286c5b84636d3a9f1de
etnt <etnt>
2005-04-13 23:07:47 +0000

    Adding file to prepare for erlmerge.

---
+++ support/create_release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Function: Run it from the jungerl/lib/$App directory
+#           in order to create a new release tar-ball.
+#
+
+CUR=`pwd`
+APP=`basename ${CUR}`
+VSN=`cat vsn.mk | sed -e 's/.*=//'`
+DIR=release/${APP}-${VSN}
+
+mkdir -p ${DIR}
+mkdir -p ${DIR}/ebin
+
+for i in `ls -d * | sed -e 's/release//g' -e 's/ebin//g'`
+do
+  [ -d ${i} ] && cp -Rp ${i} ${DIR} && rm -rf  ${DIR}/${i}/CVS
+  [ -f ${i} ] && cp -p ${i} ${DIR} 
+done
+rm -rf ${DIR}/CVS
+
+(cd release; tar cvzf ${APP}-${VSN}.tar.gz ${APP}-${VSN}; rm -rf ${APP}-${VSN})

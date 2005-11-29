e47e165ec1194e38a722c49f4d8b291cffa23893
etnt <etnt>
2005-11-29 13:47:38 +0000

    *** empty log message ***

--- lib/yfront/src/yfront.erl
+++ lib/yfront/src/yfront.erl
@@ -14,10 +14,54 @@
 
 %% External exports
 -export([authorization_hdr/1,
-	 auth_redirect/0, auth_redirect/1,
-	 auth_digest_redirect/0]).
+	 auth_redirect/0, auth_redirect/1, auth_digest_redirect/0,
+	 setup_sequence/0, init_sequence/1, init_sequence/2, 
+	 sequence/1, sequence/2]).
 
 -include("yaws_api.hrl").
+
+%%% sequence table record
+%%% (taken from http://www.erlang.org/ml-archive/erlang-questions/200508/msg00291.html)
+-record(sequence, {key, index}).
+
+%%% @doc Creates sequence table on local node.
+setup_sequence() ->
+    setup_sequence([node()]).
+
+%%% @doc Creates sequence table on Nodes.
+setup_sequence(Nodes) when list(Nodes) ->
+    create_sequence(Nodes).
+
+create_sequence(Nodes) ->
+    mnesia:create_table(sequence, [{type, set},
+				   {disc_copies, Nodes},
+				   {attributes, record_info(fields, sequence)}]).
+
+%%% @doc Initiate sequence counter to: 1
+init_sequence(Name) ->
+    init_sequence(Name, 1).
+
+%%% @doc Initiate sequence counter to: Value
+init_sequence(Name, Value) ->
+    {atomic, ok} =
+	mnesia:transaction(fun() ->
+				   mnesia:write(#sequence{key=Name, index=Value})
+			   end),
+    ok.
+
+%%% @doc Returns current value for sequence Name 
+%%%      and increment. Sequence is created if it does not exists, 
+%%%      and initial value 0 is returned.
+sequence(Name) ->
+     sequence(Name, 1).
+
+%%% @doc Increment sequence with Inc
+sequence(Name, Inc) ->
+     mnesia:dirty_update_counter(sequence, Name, Inc).
+
+
+
+
 
 
 %%% @doc Return a 401 Authorization-Required status code, require Basic-Authentication.

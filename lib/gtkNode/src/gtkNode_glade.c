07174f50ad63a86b177551246b83f5348074b1d4
masse <masse>
2006-01-06 16:25:46 +0000

    * src/gtkNode_glade.c (Module): since it seems there can be signals witout events, gtkNode can now pass the event type 'NONE' to the erlang side.
    * src/gtkNode_glade.c (Module): handle the case where the signal
    handler gets called but there is no event. have no idea why this
    can happen...

--- lib/gtkNode/src/gtkNode_glade.c
+++ lib/gtkNode/src/gtkNode_glade.c
@@ -19,15 +19,18 @@ void gn_sighandler(GtkWidget *widget){
   
   /* instantiate the GdkEventType */
   g_assert( gn_GType_from_name("GdkEventType") ); 
-
-  if ( ! (gdk_event = gtk_get_current_event()) )
-    return;
-  wid = gtk_get_event_widget(gdk_event);
-  g_assert( gn_get_enum_name("GdkEventType", gdk_event->type, evtype));
+  
   if ( ! (widgetname = glade_get_widget_name(widget)) )
     widgetname = "UNKNOWN";
+
+  if ( ! (gdk_event = gtk_get_current_event()) ){
+    g_strlcpy(evtype,"NONE",sizeof(evtype));
+  }else{
+    /*wid = gtk_get_event_widget(gdk_event);*/
+    g_assert( gn_get_enum_name("GdkEventType", gdk_event->type, evtype));
+    gdk_event_free(gdk_event);
+  }
   gn_send_signal(widgetname, evtype);
-  gdk_event_free(gdk_event);
 }
 
 GtkWidget* gn_check_widget_name(char* widget_name) {

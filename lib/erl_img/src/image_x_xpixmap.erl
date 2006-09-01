76a3e2ec531c67c47886f3c1d014a3ed5c7d383d
tonyrog <tonyrog>
2006-09-01 23:28:20 +0000

    Fixed warnings and changed to 8 bit color components

--- lib/erl_img/src/image_x_xpixmap.erl
+++ lib/erl_img/src/image_x_xpixmap.erl
@@ -22,7 +22,7 @@
 %% Read magic info check MAGIC type and width and height (depth)
 %% of image
 
-magic(<<"/* XPM */\n", Data/binary>>) -> true;
+magic(<<"/* XPM */\n", _Data/binary>>) -> true;
 magic(_) -> false.
 
 mime_type() -> "image/x-xpixmap".
@@ -40,7 +40,7 @@ read_info(Fd) ->
 				     end) of
 			{ok,Ts} ->
 			    [{string,Fmt}|_] = reverse(Ts),
-			    {ok,[Width,Height,NColors,CharPerPixel],Rest} = 
+			    {ok,[Width,Height,_NColors,_CharPerPixel],_Rest} = 
 				io_lib:fread("~d ~d ~d ~d",Fmt),
 			    {ok,#erl_image {
 			       type   = ?MODULE,
@@ -60,7 +60,7 @@ read_info(Fd) ->
     end.
 
 
-write_info(Fd, IMG) ->
+write_info(_Fd, _IMG) ->
     ok.
 
 
@@ -82,11 +82,11 @@ read(Fd,IMG) ->
 		 [{Ri,Row}|St] end, 
 	 []).
 
-write(Fd,IMG) ->
+write(_Fd,_IMG) ->
     ok.
 
 tr_rows(IMG,[Fmt|Data],RowFun,St) ->
-    {ok,[Width,Height,NColors,CharPerPixel],Rest} = 
+    {ok,[Width,Height,NColors,CharPerPixel],_Rest} = 
 	io_lib:fread("~d ~d ~d ~d",Fmt),
     {Colors,Data1} = tr_colors(Data,NColors,[],CharPerPixel),
     ColorMap = color_tab(Colors),
@@ -106,17 +106,17 @@ tr_pixels([Line|Ls],Colors,PIX,Ri,NRows,RowFun,St0) when Ri =/= NRows ->
     Row = tr_line(Line, [], Colors),
     St1 = RowFun(PIX,Row,Ri,St0),
     tr_pixels(Ls, Colors, PIX, Ri+1, NRows, RowFun, St1);
-tr_pixels([],Colors,PIX,NRows,NRows,RowFun,St0) ->
+tr_pixels([],_Colors,PIX,NRows,NRows,_RowFun,St0) ->
     {ok, PIX#erl_pixmap { pixels = St0 }};
-tr_pixels(_, Colors, IMG, _, _, RowFun, St0) ->
+tr_pixels(_, _Colors, _IMG, _, _, _RowFun, _St0) ->
     {error, bad_data}.
     
 
 color_tab([{_, " c #"++Hex} | Cs]) ->
     Color = hex_to_integer(Hex),
-    R = ((Color bsr 16) band 16#ff)*255,
-    G = ((Color bsr 8) band 16#ff)*255,
-    B = ((Color bsr 0) band 16#ff)*255,
+    R = (Color bsr 16) band 16#ff,
+    G = (Color bsr 8) band 16#ff,
+    B = (Color bsr 0) band 16#ff,
     [ {R,G,B} | color_tab(Cs)];
 color_tab([{_, " c "++Name} | Cs]) ->
     [ Name | color_tab(Cs)];
@@ -138,7 +138,7 @@ hex_to_integer([], N)  -> N.
 %% Translate a line into color index binary line
 %%
 
-tr_line([], Acc, Colors) ->
+tr_line([], Acc, _Colors) ->
     list_to_binary(reverse(Acc));
 tr_line(Line, Acc, Colors) ->
     {Line1,Index} = tr_pixel(Colors, Line, 1),
@@ -150,7 +150,7 @@ tr_pixel([{Chars,_}|Cs], Line, I) ->
 	false -> tr_pixel(Cs, Line, I+1)
     end.
 
-tr_colors(Data, 0, Acc, Cpp) ->
+tr_colors(Data, 0, Acc, _Cpp) ->
     {reverse(Acc), Data};
 tr_colors([Color|Cs], N, Acc, Cpp) ->
     {Chars,Spec} = get_color(Color,[],Cpp),
@@ -161,7 +161,7 @@ get_color([C|Cs],Acc,N) -> get_color(Cs,[C|Acc],N-1).
 
 
 %% parse the xpm file 
-parse_xpm([{id,"static"},{id,"char"},'*',{id,Name},'[', ']','=', '{' | Ts]) ->
+parse_xpm([{id,"static"},{id,"char"},'*',{id,_Name},'[', ']','=', '{' | Ts]) ->
     parse_xpm(Ts, []);
 parse_xpm(Ts) ->
     {error,{bad_format,Ts}}.
@@ -176,7 +176,7 @@ parse_xpm(Ts,_) ->
     {error, {bad_format,Ts}}.
 
 scan_xpm(Fd) ->
-    scan_xpm(Fd, fun(T) -> true end).
+    scan_xpm(Fd, fun(_) -> true end).
 
 scan_xpm(Fd, While) ->
     more(Fd, fun(Cs) -> scan_xpm(Cs,Fd,[],While) end, fun() -> {ok,[]} end).

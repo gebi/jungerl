db6943ccaf37eb6a9400de342ca750d8b837d818
richcarl <richcarl>
2008-10-24 13:24:40 +0000

    applied patch from unknown person at Kreditor, originally dated 2005-04-04, to handle 'doubles' without decimal point

--- lib/xmlrpc/src/xmlrpc_decode.erl
+++ lib/xmlrpc/src/xmlrpc_decode.erl
@@ -195,7 +195,12 @@ make_integer(Integer) ->
 
 make_double(Double) ->
     case catch list_to_float(Double) of
-	{'EXIT', _} -> throw({error, {not_double, Double}});
+	{'EXIT', _} ->
+	    case catch list_to_integer(Double) of
+		{'EXIT', _} ->
+		    throw({error, {not_double, Double}});
+		Value -> float(Value)
+	    end;
 	Value -> Value
     end.
 
